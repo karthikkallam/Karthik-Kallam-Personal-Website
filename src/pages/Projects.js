0918fbbcff4e0947979ce1a3344da7cc6dee7776
karthikkallam <kallamkkr@gmail.com>
2024-11-12 21:59:26 -0500

    Projects Update Commit

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -4,23 +4,33 @@ import './Projects.css';
 function Projects() {
   const projects = [
     {
-      title: 'Idle Incremental Game',
-      description: 'Developed an idle incremental game using Unity, involving resource management, upgrades, and progression mechanics. Players achieve continuous growth with minimal active input, driven by strategic planning and adaptive rewards, ensuring long-term engagement and player retention.',
-      technologies: 'Unity • C#',
-      icon: '../assets/icons/idle-game-icon.svg', 
+      title: 'AI Options Trading Project',
+      description: 'Created an AI-powered system for options trading that uses data acquisition, signal processing, and strategy execution. Used machine models with PyTorch to create an AI that predicts the an optimal options play. The backend, built with Flask, supports trading logic and data flow, while a React dashboard provides real-time visualization and control.',
+      technologies: 'Python • PyTorch • Flask • React',
+      icon: '../assets/icons/website-icon.png',
     },
-    {
-      title: 'Data Analysis Research',
-      description: 'Conducted data analysis research at the Ohio State University on the topic of access to healthy food on different places around the campus. After surveying 179 students across campus, uncovered campuswide flaws regarding the average distance to grocery stores and public transportation for students.',
-      technologies: 'Python • R • Pandas',
-      icon: '../assets/icons/data-analysis-icon.png',
-    },
+
     {
       title: 'AI Poker Project',
       description: 'Built an AI-powered poker coach using reinforcement learning algorithms to create a very strong AI poker player from scratch. With this expertise the AI has, if a player plays at least 100 hands with the AI, the AI can determine flaws in the players’ playstyle and offers advice to improve their game.',
       technologies: 'Python • Pytorch • Flask',
       icon: '../assets/icons/poker-icon.png',
     },
+
+    {
+      title: 'Idle Incremental Game',
+      description: 'Developed an idle incremental game using Unity, involving resource management, upgrades, and progression mechanics. Players achieve continuous growth with minimal active input, driven by strategic planning and adaptive rewards, ensuring long-term engagement and player retention.',
+      technologies: 'Unity • C#',
+      icon: '../assets/icons/idle-game-icon.svg', 
+    },
+
+    {
+      title: 'Data Analysis Research',
+      description: 'Conducted data analysis research at the Ohio State University on the topic of access to healthy food on different places around the campus. After surveying 179 students across campus, uncovered campuswide flaws regarding the average distance to grocery stores and public transportation for students.',
+      technologies: 'Python • R • Pandas',
+      icon: '../assets/icons/data-analysis-icon.png',
+    },
+
     {
       title: 'Personal Website',
       description: 'Designed and built a personal portfolio website to showcase projects and experience, featuring multiple interactive themes and animations. This website highlights my skills in front-end development, user experience, and design.',
